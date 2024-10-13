12ffeb23ab0ab0fcc47e6b35c2f929335392e812
eto0831 <eto0831@gmail.com>
2024-10-13 20:05:20 +0900

    error_massage_canged

--- src/resources/views/users/index.blade.php
+++ src/resources/views/users/index.blade.php
@@ -12,7 +12,7 @@
 <form class="search-form" action="/users/search" method="get">
   @csrf
   <div class="users-search">
-      <input type="text" class="search-form__item-input" placeholder="名前やメールアドレスを入力してください" name="keyword"
+      <input type="text" class="search-form__item-input" placeholder="名前やID、メールアドレスを入力してください" name="keyword"
           value="{{ old('keyword') }}">
   </div>
   <div class="search-form__button">
